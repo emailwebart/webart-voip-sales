37aa6e08145dd35e4974071298793ec779f913f7
WebArt Technology <email.webart@gmail.com>
2025-10-28 22:12:18 +0000

    change the colors make the better use deep blue and white and also chang

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Space Grotesk', 'sans-serif'],
+        body: ['Noto Sans', 'sans-serif'],
+        headline: ['Playfair Display', 'serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
