50192cad8ededae5c8ebe33fc7b98af27be4a415
WebArt Technology <email.webart@gmail.com>
2025-10-28 22:39:15 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'webart.technology',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
